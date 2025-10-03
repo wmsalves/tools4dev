6271afc42b751945381d8a6198cc17f61bd6a7ac
Infinity <wemersoncontas22@gmail.com>
2025-10-03 08:59:03 -0300

    fix(app): correct main app unit test to reflect new structure

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,25 +1,41 @@
-import { provideZonelessChangeDetection } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
-import { App } from './app';
+import { AppComponent } from './app.component';
+import { provideRouter } from '@angular/router';
+import { provideZonelessChangeDetection } from '@angular/core';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
-describe('App', () => {
+describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App],
-      providers: [provideZonelessChangeDetection()]
+      imports: [AppComponent, NoopAnimationsModule],
+      providers: [provideRouter([]), provideZonelessChangeDetection()],
     }).compileComponents();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(App);
+    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(App);
+  it('should render the footer correctly', () => {
+    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, tools4dev');
+    // Verifica se o elemento do rodapé contém o texto esperado
+    expect(compiled.querySelector('footer.footer small')?.textContent).toContain('Tools4Dev');
+  });
+
+  it(`should have the current year in the footer`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    const currentYear = new Date().getFullYear();
+    expect(app.year).toEqual(currentYear);
+
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('footer.footer small')?.textContent).toContain(
+      currentYear.toString()
+    );
   });
 });
